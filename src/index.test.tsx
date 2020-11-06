8119423fdc73de418752beb12efd6b5f47eec54d
garrygu <garry.guzy@gmail.com>
2020-11-06 17:16:40 +0800

    chore: init repo

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,7 +1,13 @@
-import { ExampleComponent } from '.'
+// TDD first and then enjoy coding !!!
+import { DataClient, ObjectClient } from '.'
 
-describe('ExampleComponent', () => {
-  it('is truthy', () => {
-    expect(ExampleComponent).toBeTruthy()
+describe('DataClient For Epub Data RestAPI ', () => {
+  it('DataClient Function', async () => {
+    const rootClient = new ObjectClient('/v3/api/h5/works/soskgm/form')
+    const result = await rootClient.get()
+    expect(result).toBe(require('./testResults/form.json'))
+    const dataClient = new DataClient('v3/api/h5/works/soskgm/form/objects')
+    const results = await dataClient.get()
+    expect(results).toBe(require('./testResults/formData.json'))
   })
 })
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export class DataClient {}
+
+export class ObjectClient {}
